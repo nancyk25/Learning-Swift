b211d2bbb7dbc39e15c5b4c5958ce3544daff91d
nancyk25 <nancykim@Administrators-MacBook-Air-7.local>
2014-10-02 23:05:24 -0700

    Initial Commit

---
+++ Learning Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Learning Swift
+//
+//  Created by Nancy Kim on 10/2/14.
+//  Copyright (c) 2014 Udemy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
